acf8f9d995bed75293466006b8ff3841bbc0941e
Benjamin Isasmendi <70007365+Mapacheee@users.noreply.github.com>
2025-05-13 21:19:21 -0400

    added PUT from methods to Teacher

--- src/main/java/me/mapacheee/SchoolManager/controllers/TeacherController.java
+++ src/main/java/me/mapacheee/SchoolManager/controllers/TeacherController.java
@@ -3,14 +3,26 @@ package me.mapacheee.SchoolManager.controllers;
 import me.mapacheee.SchoolManager.domains.Teacher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.servlet.function.EntityResponse;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/teacher")
 @RestController
 public class TeacherController {
+
+    private void updateFields(@RequestBody Teacher teacher, Teacher tea) {
+        if (teacher.getName() != null) {
+            tea.setName(teacher.getName());
+        }
+        if (teacher.getEmail() != null) {
+            tea.setEmail(teacher.getEmail());
+        }
+        if (teacher.getCourses() != null) {
+            tea.setCourses(teacher.getCourses());
+        }
+    }
 
     // temporaly "db" to test
     private List<Teacher> teachers = new ArrayList<Teacher>();
@@ -29,9 +41,22 @@ public class TeacherController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    /*@PutMapping("/{id}")
-    public EntityResponse<Teacher> putTeacher(@RequestBody Teacher teacher, int id) {
+    @PutMapping("/{id}")
+    public ResponseEntity<Teacher> putTeacher(@RequestBody Teacher teacher, int id) {
 
+        ResponseEntity<Teacher> re;
+        Optional<Teacher> existingTeacher = teachers.stream()
+                .filter(t -> t.getID() == id)
+                .findFirst();
 
-    }*/
+        if (existingTeacher.isPresent()) {
+
+            re = ResponseEntity.ok(existingTeacher.get());
+            updateFields(teacher, existingTeacher.get());
+
+        }
+        else { re = ResponseEntity.notFound().build(); }
+
+        return re;
+    }
 }
